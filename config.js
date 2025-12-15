00241e675c01c7d43eb65e90af87ba93236a7199
ducdao119 <ducdao11092002@gmail.com>
2025-12-15 22:39:11 +0700

    Init: Love you

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Love you',
+    title: 'Phải chăng em đã yêu anh từ ánh nhìn đầu tiên 😙',
+    desc: 'Có lẽ trái tim em đã khẽ rung lên ngay khoảnh khắc bắt gặp anh khi ấy…',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Em trai à :3',
+    question:'Trên thế giới hơn 8 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Cảm ơn anh đã đến bên em, yêu anh nhiều <3     ',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'tối thứ sáu 7h anh qua đón bé iu nhé!',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/Koios110902' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
